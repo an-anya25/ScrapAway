0968790aebc36076dea8cd90d7bfa03069cb2703
Ananya Babu <ananyababu25@gmail.com>
2024-04-05 22:40:55 +0530

    django updates

--- ScrapAway/ScrapAway/settings.py
+++ ScrapAway/ScrapAway/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
